8174d5674f759b6a880727cacc29af7343547bad
David Mynors <hello@davidmyno.rs>
2022-10-16 15:28:29 +0100

    Persist store in localStorage

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,25 @@
-import { writable } from "svelte/store";
+import { writable, type Writable } from "svelte/store";
 
 export const isSettingsOpen = writable(false);
 
-export const settings = writable({
+export const settings = persistStore("settings", {
 	alwaysStayAwake: { name: "always keep screen awake", value: false },
 });
+
+function persistStore<T>(key: string, defaultValue: T): Writable<T> {
+	let valueFromStorage;
+
+	try {
+		valueFromStorage = JSON.parse(localStorage.getItem(key));
+	} catch (e) {
+		valueFromStorage = null;
+	}
+
+	const valueToUse = writable(valueFromStorage ?? defaultValue);
+
+	valueToUse.subscribe((newValue) => {
+		localStorage.setItem(key, JSON.stringify(newValue));
+	});
+
+	return valueToUse;
+}
